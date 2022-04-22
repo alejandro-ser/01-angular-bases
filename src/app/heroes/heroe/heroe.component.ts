f3290f993a008a22f28578ce2ab4d15de3bf1971
alejandro-giraldo <alejosegi@gmail.com>
2022-04-22 22:15:48 +0200

    On way databinding

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -9,12 +9,20 @@ export class HeroeComponent {
     nombre: string = 'Ironman';
     edad: number = 45;
 
-    get nombreCapitalizado() {
+    get nombreCapitalizado(): string {
         return this.nombre.toUpperCase();
     }
 
     obtenerNombre(): string {
         return `${ this.nombre } - ${ this.edad }`;
+    }
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad(): void {
+        this.edad = 30;
     }
 
 }
