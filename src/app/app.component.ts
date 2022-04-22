736c555d9d0aea5dc987681aec9169cd5b6e00b5
alejandro-giraldo <alejosegi@gmail.com>
2022-04-22 19:14:01 +0200

    Contador con función JS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public titulo: string = 'Contador App';
   numero: number = 10;
+
+  acumular( valor: number ) {
+    this.numero += valor;
+  }
 }
