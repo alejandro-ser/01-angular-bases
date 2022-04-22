e582f0f60e07909b10d00427f1743db2cc6527fb
alejandro-giraldo <alejosegi@gmail.com>
2022-04-22 22:09:35 +0200

    Componente héroe

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent {
+
+    nombre: string = 'Ironman';
+    edad: number = 45;
+
+    get nombreCapitalizado() {
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string {
+        return `${ this.nombre } - ${ this.edad }`;
+    }
+
+}
