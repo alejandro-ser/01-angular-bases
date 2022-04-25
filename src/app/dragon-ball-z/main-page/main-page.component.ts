d830ddee3547b56c659f4b2adb8b562049e401fc
alejandro-giraldo <alejosegi@gmail.com>
2022-04-25 15:07:31 +0200

    Centralizar acceso a personajes usando services

--- src/app/dragon-ball-z/main-page/main-page.component.ts
+++ src/app/dragon-ball-z/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,24 +8,11 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
-
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
   };
 
-  agregarNuevoPersonaje( personaje: Personaje ) {
-    this.personajes.push(personaje);
-  }
+  constructor() {}
 
 }
--- src/app/dragon-ball-z/personajes/personajes.component.ts
+++ src/app/dragon-ball-z/personajes/personajes.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-personajes',
@@ -7,6 +8,12 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class PersonajesComponent {
 
-  @Input('data') personajes: Personaje[] = [];
+  // @Input('data') personajes: Personaje[] = [];
+
+  get personajes() {
+    return this.dbzService.personajes;
+  }
+
+  constructor( private dbzService: DbzService) {}
 
 }
