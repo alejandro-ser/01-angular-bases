bc5b6bfbb40cffbf24e42fe1560c8c3a48f70a2f
alejandro-giraldo <alejosegi@gmail.com>
2022-04-25 11:39:19 +0200

    @Inputs y modulos

--- src/app/dragon-ball-z/main-page/main-page.component.ts
+++ src/app/dragon-ball-z/main-page/main-page.component.ts
@@ -19,20 +19,8 @@ export class MainPageComponent {
   ];
 
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
-  }
-
-  agregar() {
-    if ( this.nuevo.nombre.trim().length === 0 ) { return; }
-
-    console.log(this.nuevo);
-
-    this.personajes.push( this.nuevo );
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    };
-  }
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  };
 
 }
