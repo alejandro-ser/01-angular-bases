6a5372c72588d4523e3291f1b17b0411d08fec21
alejandro-giraldo <alejosegi@gmail.com>
2022-04-25 10:54:10 +0200

    Agregar ngModel

--- src/app/dragon-ball-z/main-page/main-page.component.ts
+++ src/app/dragon-ball-z/main-page/main-page.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
+  cambiarNombre( event: any ) {
+    console.log(event.target.value)
+  }
+
   agregar() {
-    console.log('La prueba!')
+    console.log(this.nuevo)
   }
 
 }
